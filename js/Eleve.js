6cb539314bf4da84780cb4c8e1c025cb19bce24b
Martin Boudreau <32491185+bobanum@users.noreply.github.com>
2018-12-14 18:39:11 -0500

    Eleves fonctionelles et mise en page adaptative (plus)

--- js/Eleve.js
+++ js/Eleve.js
@@ -22,6 +22,25 @@ export default class Eleve {
 		resultat.obj = this;
 		return resultat;
 	}
+	html_radio() {
+		var resultat = document.createElement("li");
+		var radio = resultat.appendChild(document.createElement("input"));
+		radio.setAttribute("type", "radio");
+		radio.setAttribute("name", "eleve");
+		radio.setAttribute("id", "eleve_" + this.matricule);
+		radio.setAttribute("value", this.matricule);
+		radio.obj = this;
+		var label = resultat.appendChild(document.createElement("label"));
+		label.setAttribute("for", "eleve_" + this.matricule);
+		label.innerHTML = '<span className="nom"><span>' + this.nom + '</span>, <span>' + this.prenom + '</span></span> <span class="matricule">' + this.matricule + '</span>';
+		radio.addEventListener("click", function (e) {
+			e.stopPropagation();
+			e.cancelBubble = true;
+			return false;
+		});
+		resultat.obj = this;
+		return resultat;
+	}
 	static html_optgroup(nomGroupe, groupe) {
 		let resultat = document.createElement("optgroup");
 		resultat.setAttribute("label", nomGroupe);
@@ -41,11 +60,36 @@ export default class Eleve {
 			}
 		});
 		eleves.forEach(e => (e.groupe = nomGroupe, resultat.appendChild(e.html_option())));
-//		for (let matricule in groupe) {
-//			let eleve = Eleve.fromArray(groupe[matricule]);
-//			eleve.groupe = nomGroupe;
-//			resultat.appendChild(eleve.html_option());
-//		}
+		resultat.addEventListener("click", function () {
+			var disabled = this.parentNode.querySelectorAll("optgroup[disabled]");
+			if (disabled.length > 0) {
+				disabled.forEach((d)=>d.removeAttribute("disabled"));
+			} else {
+				this.parentNode.querySelectorAll("optgroup").forEach((ch)=>ch.setAttribute("disabled", "disabled"));
+				this.removeAttribute("disabled");
+			}
+		});
+		return resultat;
+	}
+	static html_radioGroup(nomGroupe, groupe) {
+		var resultat = document.createElement("fieldset");
+		var ul = resultat.appendChild(document.createElement("ul"));
+		var eleves = Object.values(groupe);
+		eleves = eleves.map((e)=>Eleve.fromArray(e));
+		eleves.sort(function (eleve1, eleve2) {
+			if (eleve1.nom < eleve2.nom) {
+				return -1;
+			} else if (eleve1.nom > eleve2.nom) {
+				return 1;
+			} else if (eleve1.prenom < eleve2.prenom) {
+				return -1;
+			} else if (eleve1.prenom > eleve2.prenom) {
+				return 1;
+			} else {
+				return 0;
+			}
+		});
+		eleves.forEach(e => (e.groupe = nomGroupe, ul.appendChild(e.html_radio())));
 		resultat.addEventListener("click", function () {
 			var disabled = this.parentNode.querySelectorAll("optgroup[disabled]");
 			if (disabled.length > 0) {
@@ -88,6 +132,20 @@ export default class Eleve {
 		resultat.addEventListener("change", this.evt.selectEleve.change);
 		return resultat;
 	}
+	static html_radioEleves(eleves) {
+		eleves = eleves || this.eleves || {};
+		var resultat = document.createElement("fieldset");
+		let ul = resultat.appendChild(document.createElement("ul"));
+		for (let nomGroupe in this.eleves) {
+			let li = ul.appendChild(document.createElement("li"));
+			let h2 = li.appendChild(document.createElement("h2"));
+			h2.addEventListener("click", e => e.target.classList.toggle("folded"));
+			h2.innerHTML = nomGroupe;
+			li.appendChild(this.html_radioGroup(nomGroupe, this.eleves[nomGroupe]));
+		}
+		resultat.addEventListener("change", this.evt.selectEleve.change);
+		return resultat;
+	}
 	static fromArray(a) {
 		var resultat = new Eleve();
 		resultat.matricule = a[0];
@@ -97,7 +155,7 @@ export default class Eleve {
 	}
 	static loadAll(cours, annee) {
 		this.eleves = null;
-		var conteneur = document.createElement("section");
+		var conteneur = document.createElement("form");
 		conteneur.classList.add("eleves");
 		var titre = conteneur.appendChild(document.createElement("h1"));
 		titre.innerHTML = "Les élèves";
@@ -108,7 +166,7 @@ export default class Eleve {
 		};
 		GValue.callApi(data).then(json => {
 			Eleve.eleves = json;
-			conteneur.appendChild(Eleve.html_selectEleve());
+			conteneur.appendChild(Eleve.html_radioEleves());
 		});
 		return conteneur;
 	}
@@ -133,7 +191,7 @@ export default class Eleve {
 		this.evt = {
 			selectEleve: {
 				change: function (e) {
-					var choix = e.target.selectedOptions[0].obj;
+					var choix = e.target.obj;
 					choix.loadResultat().then(data => {
 						GValue.resultat = data;
 						data.appliquer();
